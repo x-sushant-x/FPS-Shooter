c361f24ddcea41f0b4429136cd629fd3b5418684
Sushant <sushant.dhiman9812@gmail.com>
2024-02-18 09:58:44 +0530

    Added Smooth FPS Camera

---
+++ Scenes/SmoothCamera.gd
@@ -0,0 +1,13 @@
+""" PoF -> This file will smoothen the FPS Camera.
+Rotation on y axis is handled by Player.gd But for x axis we have a dummy node as a parent of camera which rotates in x axis.
+To smoothen this we will blend it. """
+
+extends Camera3D
+var speed : float = 44.0
+
+func _physics_process(delta):
+	var weight : float = delta * speed
+	
+	global_transform.interpolate_with(
+		get_parent().global_transform, weight 
+	)
